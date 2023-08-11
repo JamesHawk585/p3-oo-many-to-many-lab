6630a7815979e0aa3e0cc110288d6246d08caac6
James Hawk <jph94880@gmail.com>
2023-08-10 20:22:53 -0500

    Increments Author.count += 1

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,40 @@
 class Author:
-    pass
+    count = 0
+    def __init__(self, name):
+        self.name = name
+        Author.add_to_author_count()
+    def add_to_author_count():
+        Author.count += 1 
+
+# The Author class should have the following methods:
+#  []  contracts(self): This method should return a list of related contracts.
+#  [] books(self): This method should return a list of related books using the Contract class  as an intermediary.
+#  [] sign_contract(book, date, royalties): This method should create and return a new Contract object between the author and the specified book with the specified date and royalties
+#  [] total_royalties(): This method should return the total amount of royalties that the author has earned from all of their contracts.
+
+
+james_hawk = Author("James Hawk")
+mason_hawk = Author("Mason Hawk")
 
 
 class Book:
-    pass
+    count = 0 
+    def __init__(self, title):
+        self.title = title
 
 
-class Contract:
-    pass
+class Contract(Author, Book):
+    count = 0
+    def __init__(self, book, author, date, royalties):
+        self.book = book
+        self.author = author
+        self.date = date
+        self.royalties = royalties
+
+# The author property should be an instance of the Author class, while the book property should be an instance of the Book class. The date property should be a string that represents the date when the contract was signed, while the royalties property should be a number that represents the percentage of royalties that the author will receive for the book.
+
+# The Contract class should have the following methods:
+
+# [] A class method contracts_by_date(cls, date): This method should return all contracts that have the same date as the date passed into the method.
+
+        
